295c531c4f3fc1f4b5417a5bfda9d056a1123a5f
Herbert Joseph <techniquea2z@gmail.com>
2025-08-06 17:48:04 -0400

    fix(enum): syntax

--- app/models/linked_account.rb
+++ app/models/linked_account.rb
@@ -1,6 +1,5 @@
 class LinkedAccount < ApplicationRecord
   belongs_to :user
 
-  enum :account_type, { bank: 0, fsa: 1, hsa: 2, credit_card: 3 }
-
+  enum account_type: { bank: 0, fsa: 1, hsa: 2, credit_card: 3 }
 end
